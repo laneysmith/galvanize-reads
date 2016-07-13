cc3047613d79a1c1af89d5d3e371474232a9021a
Laney Smith <lsmith.unc@gmail.com>
2016-07-13 15:59:46 -0600

    Show results count for authors & books pages

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
+  var bookCount = $(".card:visible").length;
+  $(".book-count").text(bookCount);
+  var authorCount = $(".card").length;
+  $(".author-count").text(authorCount);
   $('#genre-filter').change(function(){
     var selectedGenre = $('#genre-filter :selected').text();
     if (selectedGenre === "Show All Genres") {
       $(".card").show();
+      bookCount = $(".card:visible").length;
+      $(".book-count").text(bookCount);
     } else {
       $(".card[data-tag='" + selectedGenre + "']").show();
       $(".card:not([data-tag='" + selectedGenre + "'])").hide();
+      bookCount = $(".card:visible").length;
+      $(".book-count").text(bookCount);
     }
   });
 });
